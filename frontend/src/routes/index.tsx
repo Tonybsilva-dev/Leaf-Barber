0bb1aa40288a57f4e3f5e04306d8ac3bebb84c29
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-06-03 14:50:11 -0300

    AuthRoutes and Locations ok

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-
+import { Switch } from 'react-router-dom'
+import Route from './Route'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SingUp'
+import Dashboard from '../pages/Dashboard'
+
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={SignIn} />
     <Route path="/signUp" component={SignUp} />
+
+    <Route path="/dash" component={Dashboard} isPrivate />
+
   </Switch>
 )
 

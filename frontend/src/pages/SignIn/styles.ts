bf2ee2ec5a9e1268def6c2bf666b6e5c17e99f35
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-06-03 12:45:30 -0300

    Keyframes added

--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import SingInBackgroundIMG from '../../assets/sign-in-background.png'
 import { shade } from 'polished'
 
@@ -17,7 +17,33 @@ export const Content = styled.div`
   width: 100%;
   max-width: 700px;
 
-  form {
+`
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${SingInBackgroundIMG}) no-repeat center;
+  background-size: cover;
+`
+
+const apperFromLeft = keyframes`
+from{
+  opacity: 0;
+  transform: translateX(-50px);
+}to{
+  opacity: 1;
+  transform: translateX(0);
+}
+`
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${ apperFromLeft } 1s;
+
+form {
     margin: 80px 0;
     width: 340px;
     text-align: center;
@@ -51,10 +77,4 @@ export const Content = styled.div`
         color: ${shade(0.2, '#FF9000')};
       }
   }
-`
-
-export const Background = styled.div`
-  flex: 1;
-  background: url(${SingInBackgroundIMG}) no-repeat center;
-  background-size: cover;
 `
--- frontend/src/pages/SingUp/index.tsx
+++ frontend/src/pages/SingUp/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useRef } from 'react'
 import logo from '../../assets/logo.svg'
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
@@ -8,6 +8,7 @@ import * as Yup from 'yup'
 import getValidationErrors from '../../utils/getValidationErrors'
 import Input from '../../components/Input';
 import Button from '../../components/Button'
+import { Link } from 'react-router-dom'
 
 
 const SignUn: React.FC = () => {
@@ -34,6 +35,8 @@ const SignUn: React.FC = () => {
 
       const errors = getValidationErrors(error)
       formRef.current?.setErrors(errors)
+
+      return
     }
   }, [])
 
@@ -41,18 +44,20 @@ const SignUn: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <img src={logo} alt="logo" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
-          <Input type="text" name="name" placeholder="Nome Completo" icon={FiUser} autoComplete="off" />
-          <Input type="text" name="email" placeholder="Email" icon={FiMail} autoComplete="off" />
-          <Input type="password" name="password" placeholder="Senha" icon={FiLock} autoComplete="off" />
-          <Button type="submit">Cadastrar</Button>
-        </Form>
-        <a href="logon">
-          <FiArrowLeft />
+        <AnimationContainer>
+          <img src={logo} alt="logo" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
+            <Input type="text" name="name" placeholder="Nome Completo" icon={FiUser} autoComplete="off" />
+            <Input type="text" name="email" placeholder="Email" icon={FiMail} autoComplete="off" />
+            <Input type="password" name="password" placeholder="Senha" icon={FiLock} autoComplete="off" />
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+          <Link to="/">
+            <FiArrowLeft />
         Voltar para logon
-        </a>
+        </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   )
